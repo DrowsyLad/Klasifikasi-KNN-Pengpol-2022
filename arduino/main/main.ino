44eb098dd900e4d45502b8f66b0b66f2b19e36ad
DrowsyLad <tsumwashere@student.ub.ac.id>
2022-12-08 20:16:09 +0700

    Fixed bugs

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -3,16 +3,8 @@
 #include <BluetoothSerial.h>
 // #include <Servo.h>
 
-#define LOADCELL
-// #define SERVO
-#define LCD
-#define ULTRASONIC
 
 //Pin Define
-#define LOADCELL_DOUT_PIN  3
-#define LOADCELL_SCK_PIN  4
-#define SERVO_ATAS 12
-#define SERVO_BAWAH 2
 #define S0_CS 14
 #define S1_CS 12
 #define S2_CS 26
@@ -23,14 +15,10 @@
 #define trigPin 32
 
 // Class object init
-// HX711 scale;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 BluetoothSerial SerialBT;
 
 //Calibration State
-//Load Cell
-float calibration_factor = 360;
-//Color Sensor
 int redMin = 76;
 int greenMin = 86;
 int blueMin = 55;
@@ -56,53 +44,18 @@ static unsigned long t_awal;
 static unsigned long t_setelah;
 static unsigned long treshold = 2000;
 
-void send_data(int size, int red, int green, int blue){
-  delay(500);
-//  Serial.println('s');
-  String data = "";
-  data = (String(size) + "," + String(red) + "," + String(green) + "," + String(blue));
-  delay(500);
-  Serial.println(data);
-//  while(!(Serial.read() == 'f'));
-}
-
 void reset_all(){
   lcd.clear();
   lcd.setCursor(0,0);
-//  redValue = 0;
-//  greenValue = 0;
-//  blueValue = 0;
-//  myServo_bot.write(90);
-//  myServo_top.write(00);
   i = 0;
 }
-
-// void process_bottom_servo(char output){   
-//   delay(500);
-//   (output == 'S') ? myServo_bot.write(90 - 70) : myServo_bot.write(90 + 70);
-//   delay(500);
-// }
-
-// void servo_init(){
-//   myServo_bot.attach(2);
-//   myServo_top.attach(12);
-//   myServo_top.write(00);
-//   myServo_bot.write(90);
-// }
-
-//void process_top_servo(){
-//  delay(500);
-//  myServo_top.write(150);
-//  delay(500);
-//  myServo_top.write(00);
-//}
 
 char receive_data(){
   String x;
   char jenis;
   while(!Serial.available());
   x = Serial.readStringUntil('\n');
-  (x == "s") ? jenis = 'S' : jenis = 'K';
+  (x == "s") ? jenis = 'S' : jenis = 'A';
   return jenis;
 }
 
@@ -163,23 +116,6 @@ void readRGB(float* redValue, float* blueValue, float* greenValue){
 
 }
 
-// int cekWeight(){
-//   int berat = 0;
-//   scale.set_scale(calibration_factor);
-//   berat = scale.get_units();
-//   return berat;
-// }
-
-// int readWeight(){
-//   average = 0;
-//   scale.set_scale(calibration_factor);
-//   for(int i = 0; i < 5; i++){
-//     average += scale.get_units();
-//   }
-//   average /= 5;
-//   return average;
-// }
-
 float readSize(){
   long duration; // variable for the duration of sound wave travel
   float distance; // variable for the distance measurement
@@ -209,17 +145,6 @@ void setup_LCD(){
 //  lcd.begin(16,2);
   lcd.init();
   lcd.backlight();
-}
-
-void setup_loadCell(){
-  // #ifdef LOADCELL
-  // scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  // scale.set_scale();
-  // scale.tare(); //Reset the scale to 0
-  // long zero_factor = scale.read_average(); //Get a baseline reading
-  // Serial.println("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
-  // Serial.println(zero_factor);
-  // #endif
 }
 
 void setup_colorSensor(){
@@ -275,6 +200,18 @@ void printLines(String line1, String line2 = ""){
   lcd.print(line2);
 }
 
+void send_data(int size, int red, int green, int blue){
+  delay(500);
+  // Serial.println("s");
+  delay(50);
+  String data = "";
+  data = String(size) + "," + String(red);// + "," + String(green) + "," + String(blue);
+  delay(500);
+  Serial.println(data);
+  printLines("Waiting for", "server reply..");
+  while(!(Serial.read() == 'f'));
+}
+
 bool receiveCommand(String* command){
   if(SerialBT.available()){
     *command = SerialBT.readString();
@@ -310,183 +247,76 @@ void ambilDataset(){
   printLines("Restarting..");
   delay(1000);
 }
-/*
-void FSM(){
-  t_setelah = millis() - t_awal;
-  float ultrasonic_output = 0, redValue = 0, greenValue = 0, blueValue = 0;
-  switch(state){
-    case 0:
-      if(i == 0){
-        lcd.print("Put object on");
-        lcd.setCursor(0,1);
-        lcd.print("top of Load Cell");
-        i++;
-      }
-      Serial.println("Put object on top of load cell");
-      if(cekWeight() > 2 && t_setelah > treshold * 4){
-        lcd.clear();
-        lcd.setCursor(0,0);
-        lcd.print("Dont move object");
-        lcd.setCursor(0,1);
-        lcd.print("Processing...");
-        state = 1;
-        t_awal = millis();   
-      }
-      break;
 
-    case 1:      
-      if(t_setelah > treshold && cekWeight() > 2){
-        
-        lcd.clear();
-        lcd.setCursor(1,0);
-        lcd.print("Reading");
-        delay(500);
-        lcd.print(".");
-        delay(500);
-        lcd.print(".");
-        delay(500);
-        lcd.print(".");
-        delay(500);
-        lcd.setCursor(1,1);
-        lcd.print("Size: ");
-        lcd.print(readSize());
-        delay(500);
-        lcd.clear();
-        lcd.setCursor(0,0);
-        lcd.print("Weight:");
-        Serial.println("Begin reading weight...");
-        Serial.println("Reading weight...");
-        int w = readWeight();
-        Serial.print("Weight: ");
-        Serial.print(String(w) + "gram");
-        lcd.print(String(w) + " Gram");
-        delay(2000);
-        state = 2;
-        t_awal = millis();
-      }
-      break;
-
-    case 2:
-      if(t_setelah > treshold && cekWeight() > 2){
-        lcd.clear();
-        lcd.setCursor(0,0);
-        lcd.print("Color:");
-        Serial.println("Begin reading color...");
-        Serial.println("Reading color...\n");
-        Serial.print("Color:\n");
-        readRGB(&redValue, &greenValue, &blueValue);
-        lcd.setCursor(0,1);
-        lcd.print("R:");
-        lcd.print(String(redValue));
-        delay(1000);
-        lcd.print(",G:");
-        lcd.print(String(greenValue));
-        delay(1000);
-        lcd.print(",B:");
-        lcd.print(String(blueValue));
-        delay(3000);
-        state = 3;
-        t_awal = millis();
-      }
-      break;
-
-    case 3:
-      if(t_setelah > treshold){
-        lcd.clear();
-        lcd.setCursor(0,0);
-        lcd.print("Begin Classifica-");
-        lcd.setCursor(0,1);
-        lcd.print("tion...");
-        Serial.println("Begin classification...");
-        send_data(redValue, ultrasonic_output);
-        output = receive_data();
-        if(output == 'S'){
-          lcd.clear();
-          lcd.setCursor(0,0);
-          lcd.print("Jeruk");
-          lcd.setCursor(0,1);
-          lcd.print("Santang Madu");
-        } else {
-          lcd.clear();
-          lcd.setCursor(0,0);
-          lcd.print("Jeruk");
-          lcd.setCursor(0,1);
-          lcd.print("Kunci");
-        }
-        state = 4;
-        t_awal = millis();
-      }
-
-    case 4:
-      if((t_setelah > treshold) && (output == 'K' || output == 'S')){
-        Serial.print("Servo Bottom move...");
-        process_bottom_servo(output);
-        state = 5;
-        t_awal = millis();
-      }
-
-    case 5:
-      if(t_setelah > treshold){
-        Serial.print("Servo Top move...");
-        process_top_servo();
-        state = 6;
-        t_awal = millis();
-      }
-
-    case 6:
-      if(t_setelah > treshold * 2){
-        Serial.print("End of process...");
-        if(cekWeight() < 3){
-          reset_all();
-          state = 0;
-          t_awal = millis();
-        }
-      }
-      break;
-    }
-  } 
-*/
+void mainProcess(){
+  String command = "None";
+  printLines("Place your", "object inside");
+  delay(2000);
+  printLines("Press button", "to start!");
+  while(command != "Start"){
+    receiveCommand(&command);
+    delay(50);
+  }
+  command = "None";
+  float size, red, green, blue;
+  printLines("Reading size..");
+  delay(1000);
+  size = readSize();
+  printLines("Reading colors..");
+  readRGB(&red, &green, &blue);
+  printLines("Sending data", "to server..");
+  send_data(size, red, green, blue);
+  delay(500);
+  printLines("Data accepted", "by server");
+  char outputKNN = receive_data();
+  String kelasKNN;
+  if(outputKNN == 'S') kelasKNN = "Stroberi";
+  else kelasKNN = "Apel Batu";
+  printLines("Hasil klasifikasi:", kelasKNN);
+  delay(5000);
+  printLines("Press button", "to restart");
+  while(command != "Start"){
+    receiveCommand(&command);
+    delay(50);
+  }
+  printLines("Restarting..");
+  delay(1000);
+}
 
 void setup(){
   Serial.begin(9600);
   Serial.setTimeout(1);
-  // while(!Serial.available()) {
-  //   Serial.println("waiting to start...");
-  //   delay(1000);
-  // }
   setup_LCD();
   lcd.setCursor(0,0);
-  Serial.println("LCD OK...");
+  printLines("LCD OK...");
   delay(1000);
   SerialBT.begin("ESP32_Pengpol");
   printLines("Bluetooth device", "initialized..");
   delay(1000);
+  while(!Serial.available()) {
+    // Serial.println("Waiting to start...");
+    printLines("Connecting to", "Server..");
+    delay(1000);
+  }
   lcd.clear();
   lcd.print("Setting Up...");
-  Serial.println("Setting up...");
+  // Serial.println("Setting up...");
   delay(1000);
-  // setup_loadCell();
-  // Serial.println("Load Cell OK...");
-  // delay(1000);
   setup_ultrasonicSensor();
-  Serial.println("Ultrasonic Sensor OK...");
+  printLines("Ultrasonic Sensor", "OK...");
   delay(1000);
   setup_colorSensor();
-  Serial.println("Color Sensor OK...");
+  printLines("Color Sensor", "OK...");
   delay(1000);
-  // servo_init();
-  // Serial.println("Servo OK...");
-  // delay(1000);
   t_awal = millis();
   lcd.clear();
   lcd.print("All set, Ready!");
   delay(2000);
-  Serial.println("All set, Ready!");
-  delay(100);
   lcd.clear();
 }
 
 void loop(){
   // FSM();
-  ambilDataset();
+//  ambilDataset();
+  mainProcess();
 }
--- python/main/main.py
+++ python/main/main.py
@@ -16,7 +16,7 @@ ser = serial.Serial(SERIAL_PORT, BAUDRATE, timeout=1)
 
 # Argument parser for n neighbours from terminal argument
 ap = argparse.ArgumentParser()
-ap.add_argument("-n", "--neighbours", type=int, default=3, help="set the number of neighbours for voting purpose")
+ap.add_argument("-n", "--neighbours", type=int, default=5, help="set the number of neighbours for voting purpose")
 arguments = vars(ap.parse_args())
 
 # Variable for data
@@ -26,45 +26,42 @@ incoming_bytes = b''
 
 def read_data():
     global indep_data, dep_data
-    data_path = 'C:/Data/Project/Pengpol/python/asset/jeruk.csv'
+    data_path = 'C:/Data/Project/Pengpol/python/asset/KNN.csv'
     df = pd.read_csv(data_path)
     # memisahkan data independent (fitur) dan dependent (class)
-    indep_data = df.iloc[:, 1:5]
-    dep_data = df.iloc[:, 5]
+    indep_data = df.iloc[:, 1:3]
+    dep_data = df.iloc[:, 3]
 
 def process_incoming_data(data):
-    if(data == b's\r\n'):
-        arduino_data = ""
-        arr = []
+    print("Incoming data from ESP32...")
+    print(data)
+    
+    arduino_data = ""
+    arr = []
+    data = ser.readline().decode('ascii')
+    while(not data):
         data = ser.readline().decode('ascii')
-        while(not data):
-            data = ser.readline().decode('ascii')
-            pass
-        data = data[0:len(data)-2]
-        for x in data:
-            arduino_data += x
-        arr = arduino_data.split(",")
-        for i in range(0, len(arr)):
-            arr[i] = int(arr[i])
-        print(arr)
-        return knn_classifier(arr, arguments["neighbours"])
-    else:
-        print(data)
-        while(not data):
-            data = ser.readline().decode('ascii')
-            pass
-        return None 
+        pass
+    data = data[0:len(data)-2]
+    for x in data:
+        arduino_data += x
+    arr = arduino_data.split(",")
+    for i in range(0, len(arr)):
+        arr[i] = int(arr[i])
+    print(arr)
+    return knn_classifier(arr, arguments["neighbours"])
 
 def knn_classifier(data, neighbours):
     global indep_data, dep_data, output
     knn = neighbors.KNeighborsClassifier(n_neighbors=neighbours, weights="uniform")
     knn.fit(indep_data, dep_data)
     predict_knn = knn.predict([data])
-    if predict_knn == "Kunci":
+    print(predict_knn)
+    if predict_knn == "Apel":
         time.sleep(1)
         ser.write(b'f')
         time.sleep(1)
-        ser.write("k".encode())
+        ser.write("a".encode())
         print("Apel")
     else:
         time.sleep(1)
